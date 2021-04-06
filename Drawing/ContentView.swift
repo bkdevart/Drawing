10a5de16d867a887b3da6fa825515f775ece4cfe
Brandon Knox <bknox.dev@gmail.com>
2021-04-06 12:11:46 -0700

    solving rough edge path problem using rounded edges

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -14,9 +14,9 @@ struct ContentView: View {
             path.addLine(to: CGPoint(x: 100, y: 300))
             path.addLine(to: CGPoint(x: 300, y: 300))
             path.addLine(to: CGPoint(x: 200, y: 100))
-            path.addLine(to: CGPoint(x: 100, y: 300))  // closes shape completely
         }
-        .stroke(Color.blue.opacity(0.25), lineWidth: 10)
+        // using rounded corners solves rough edge problem
+        .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
     }
 }
 
