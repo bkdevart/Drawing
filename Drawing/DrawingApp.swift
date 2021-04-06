8e32ef993223ffea29a7f46cc98857d95e95d685
Brandon Knox <bknox.dev@gmail.com>
2021-04-06 11:59:00 -0700

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Brandon Knox on 4/6/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
